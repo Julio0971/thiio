fbb2416591b8364ed09f46a7d8bb8cbd35e285a8
Julio Adrián <dk.jmv.0971@gmail.com>
2024-02-24 12:48:06 -0600

    Users module created

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index () {
+        return User::paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store (Request $request) {
+        $data = $request->validate([
+            'name' => ['required', 'string'],
+            'username' => ['required', 'string', 'unique:users'],
+            'password' => ['required', 'string', 'min:8']
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+
+        User::create($data);
+
+        return response()->json([
+            'message' => 'User created successfully!'
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show (User $user) {
+        return response()->json(compact('user'), 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update (Request $request, User $user) {
+        $data = $request->validate([
+            'name' => ['required', 'string'],
+            'username' => ['required', 'string', Rule::unique('users')->ignore($user)],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
+
+        $user->name = $data['name'];
+        $user->username = $data['username'];
+        $user->password = Hash::make($data['password']);
+        $user->save();
+
+        return response()->json([
+            'message' => 'User updated successfully!'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy (User $user) {
+        $user->delete();
+
+        return response()->json([
+            'message' => 'User deleted successfully!'
+        ], 200);
+    }
+}
--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -19,7 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
     ];
 
@@ -39,7 +39,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 }
